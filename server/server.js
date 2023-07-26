d061d5f6131913bd2aeb7a3d9920382ec4fa0398
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2023-07-26 12:06:48 +0800

    initialize vite react ts

--- server/server.js
+++ server/server.js
@@ -12,41 +12,77 @@ app.use(express.json());
 
 // ================================== School year api ==================================
 app.get("/api/school-year", async (req, res) => {
-    const results = await db.query("SELECT * FROM school_year;");
-    console.log(results);
-    res.json({
-        status: "success",
-        results: results.rows.length,
-        school_years: results.rows
-    })
+    try {
+        const results = await db.query("SELECT * FROM school_year ORDER BY id;");
+        res.json({
+            status: "success",
+            results: results.rows.length,
+            school_years: results.rows
+        })
+    } catch (e) {
+        res.status(500).send();
+    }
 })
 
 app.get("/api/school-year/:id", async (req, res) => {
-    const result = await db.query(`SELECT * FROM school_year WHERE id = ${req.params.id}`);
-    res.status(201).json({
-        status: "sucess",
-        result: result.rows
-    })
+    try {
+        const result = await db.query("SELECT * FROM school_year WHERE id = $1", [req.params.id]);
+        res.status(201).json({
+            status: "sucess",
+            result: result.rows
+        })
+    } catch (e) {
+        res.status(500).send();
+    }
 })
 
-app.post("/api/school-year", (req, res) => {
-    res.status(200).json({
-        status: "success"
-    })
+app.post("/api/school-year", async (req, res) => {
+    try {
+        const results = await db.query(
+            "INSERT INTO school_year (name) VALUES ($1) returning *;",
+            [req.body.name]
+        )
+        res.status(200).json({
+            status: "success",
+            results: results.rows[0]
+        })
+    } catch (e) {
+        console.log(e)
+        res.status(500).send();
+    }
 })
 
-app.put("/api/school-year/:id", (req, res) => {
-    res.status(200).json({
-        status: "success",
-        modified_id: req.params.id
-    })
+app.put("/api/school-year/:id", async (req, res) => {
+    try {
+        const results = await db.query(
+            "UPDATE school_year SET name = $1 WHERE id = $2 returning *;",
+            [req.body.name, req.params.id]
+        )
+        res.status(200).json({
+            status: "success",
+            modified_id: req.params.id,
+            results: results.rows[0]
+        })
+    } catch (e) {
+        console.log(e);
+        res.status(500).send();
+    }
 })
 
-app.delete("/api/school-year/:id", (req, res) => {
-    res.status(204).json({
-        status: "success",
-        deleted_id: req.params.id
-    })
+app.delete("/api/school-year/:id", async (req, res) => {
+    try {
+        await db.query(
+            "DELETE FROM school_year WHERE id = $1;",
+            [req.params.id]
+        )
+        res.status(200).json({
+            status: "success",
+            modified_id: req.params.id
+        })
+    } catch (e) {
+        console.log(e);
+        res.status(500).send();
+    }
 })
 
 app.listen(port, () => {
