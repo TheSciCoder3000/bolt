bee8829070322b52e75c6a9e65fbc45ba695cdf5
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2023-08-18 19:32:17 +0800

    adding context menu to calendar route

--- client/src/components/calendar/DayTaskList.tsx
+++ client/src/components/calendar/DayTaskList.tsx
@@ -1,26 +1,73 @@
 import { fetchTasksByMonth } from 'api/task';
-import React, { LegacyRef, useEffect } from 'react'
+import TaskItemContextMenu, { CbFromContext, DataFromContext } from 'components/modal/TaskItemContextMenu';
+import React, { LegacyRef, useEffect, useState } from 'react'
 import { useOverflowDetector } from 'react-detectable-overflow';
 
-type TaskListT = Awaited<ReturnType<typeof fetchTasksByMonth>>
+type TaskListT = Awaited<ReturnType<typeof fetchTasksByMonth>>;
+const initialContextMenuState = {
+    show: false,
+    x: 0,
+    y: 0,
+    id: null as string | null,
+    completed: false
+}
+
 interface DayTaskListProps {
     tasks: TaskListT;
 }
 const DayTaskList: React.FC<DayTaskListProps> = ({ tasks }) => {
     const { ref, overflow } = useOverflowDetector({});
+    const [contextMenu, setContextMenu] = useState(initialContextMenuState);
+
 
     useEffect(() => {
         console.log(overflow)
     }, [overflow])
 
+    const contextMenuHandler = (e: React.MouseEvent<HTMLElement, MouseEvent>, id: string, completed: boolean) => {
+        e.preventDefault();
+        console.log(id, completed)
+    
+        setContextMenu({ show: true, x: e.pageX, y: e.pageY, id, completed })
+    }
+
+    const updateFromContext = (data: null | DataFromContext | CbFromContext) => {
+        if (contextMenu.id === null) return
+        const taskData = tasks.find(item => item.id === contextMenu.id);
+        const taskIndx = tasks.findIndex(item => item.id === contextMenu.id)
+        if (!taskData || taskIndx === -1) return
+    
+        if (data === null) {
+          // delete task
+        } else {
+          // update task
+        }
+    }
+
+    const closeContextMenu = () => setContextMenu(initialContextMenuState);
+
     return (
         <div ref={ref as LegacyRef<HTMLDivElement>} className='relative h-full space-y-1 overflow-hidden'>
             {tasks.map(task => (
-                <div className='bg-black text-white text-xs truncate px-2 py-1' key={task.id}>{task.name}</div>
+                <div 
+                    onContextMenu={e => contextMenuHandler(e, task.id, task.completed)} 
+                    className='bg-black text-white text-xs truncate px-2 py-1' 
+                    key={task.id}>
+                        {task.name}
+                </div>
             ))}
             {overflow && <button className='absolute left-0 right-0 bottom-0 bg-white'>
                 More {">"}
             </button>}
+            {contextMenu.show && (
+                <TaskItemContextMenu 
+                    completed={contextMenu.completed} 
+                    updateTask={updateFromContext} 
+                    x={contextMenu.x} y={contextMenu.y} 
+                    closeContextMenu={closeContextMenu}
+                    filter={["edit"]} />
+            )}
+
         </div>
     )
 }
--- client/src/components/modal/TaskItemContextMenu.tsx
+++ client/src/components/modal/TaskItemContextMenu.tsx
@@ -1,7 +1,6 @@
 import React, { useRef } from 'react'
 import Modal from '.'
 import { useContextMenu } from 'hooks/todo';
-import { CbFromContext, DataFromContext } from 'components/todo/TodoList';
 
 
 interface TaskItemContextMenuProps {
@@ -9,11 +8,18 @@ interface TaskItemContextMenuProps {
     y: number;
     completed: boolean;
     closeContextMenu: () => void;
-    updateTask?: (data: DataFromContext | null | CbFromContext) => void
+    updateTask?: (data: DataFromContext | null | CbFromContext) => void;
+    filter?: ("edit" | "toggle" | "delete")[]
 }
 
+export interface DataFromContext {
+    name: string;
+    completed: boolean;
+}
+export type CbFromContext = (state: DataFromContext) => DataFromContext
 
-const TaskItemContextMenu: React.FC<TaskItemContextMenuProps> = ({ x, y, completed, closeContextMenu, updateTask }) => {
+
+const TaskItemContextMenu: React.FC<TaskItemContextMenuProps> = ({ x, y, completed, closeContextMenu, updateTask, filter }) => {
     const contextRef = useRef<HTMLDivElement>(null)
     useContextMenu(contextRef, closeContextMenu)
 
@@ -27,14 +33,17 @@ const TaskItemContextMenu: React.FC<TaskItemContextMenuProps> = ({ x, y, complet
 
     const ContextMenuOptions = [
         {
+            id: "edit",
             name: "Edit",
             data: undefined
         },
         {
+            id: "toggle",
             name: completed ? "Mark Unfinished" : "Mark Finished",
             data: (state: DataFromContext) => ({ ...state, completed: !completed })
         },
         {
+            id: "delete",
             name: "Delete",
             data: null
         },
@@ -45,7 +54,7 @@ const TaskItemContextMenu: React.FC<TaskItemContextMenuProps> = ({ x, y, complet
                 ref={contextRef} 
                 style={{ top: `${y}px`, left: `${getModalX()}px` }}
                 className='absolute bg-gray-50 border rounded-md  shadow-md' >
-                {ContextMenuOptions.map((opt, indx) => (
+                {ContextMenuOptions.filter(item => filter ? !filter.includes(item.id as ("edit" | "toggle" | "delete")) : true).map((opt, indx) => (
                     <button 
                         key={indx} 
                         className='px-3 py-1.5 flex justify-start hover:bg-gray-200/60 w-full'
