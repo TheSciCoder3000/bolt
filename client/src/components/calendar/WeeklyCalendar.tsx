69cc5901d5945174fa4ef714dfe91a17122982e9
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2023-08-18 19:31:41 +0800

    dividing calendar views

---
+++ client/src/components/calendar/WeeklyCalendar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+interface WeeklyCalendarProps {
+
+}
+const WeeklyCalendar: React.FC<WeeklyCalendarProps> = () => {
+    return (
+        <div>WeeklyCalendar</div>
+    )
+}
+
+export default WeeklyCalendar
