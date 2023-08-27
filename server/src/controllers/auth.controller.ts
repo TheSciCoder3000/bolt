9e9a3000d5dd24949c38efd1db76f65dd8624a02
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2023-08-27 20:56:15 +0800

    updating auth controller

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -1,7 +1,8 @@
 import { PassportStatic } from "passport";
-import db from "../model";
 import bcrypt from "bcryptjs";
 import { NextFunction, Request, Response } from "express";
+import { UserRepository } from "../model/setup";
+import { User } from "../model/User.model";
 
 declare module 'express-session' {
     export interface SessionData {
@@ -27,19 +28,24 @@ export default {
     register: () => async (req: Request, res: Response) => {
         bcrypt.hash(req.body.password, 10)
             .then(async (hashPassword) => {
-                db.query("SELECT * FROM bolt_user WHERE username = $1;", [req.body.username])
-                    .then(user => {
-                        if (user.rows.length > 0) res.status(409).json({ status: "Conflict", msg: "User already exists" });
-                        else {
-                            db.query(
-                                "INSERT INTO bolt_user (username, password) VALUES ($1, $2);", 
-                                [req.body.username, hashPassword]
-                            )
-                            .then(() => res.status(201).json({ status: "success", msg: "User successfuly created" }))
-                            .catch(() => res.status(500).json({ status: "Db error", msg: "unable to insert user to db" }))
-                        }
-                    })
-                    .catch(e => res.status(500).json({ status: "Db error", msg: "unable to find user in db" }))
+                try {
+                    const user = await UserRepository.findOne({ where: { username: req.body.username } });
+                    if (user) res.status(409).json({ status: "Conflict", msg: "User already exists" });
+                    else {
+                        UserRepository
+                            .createQueryBuilder("user")
+                            .insert()
+                            .into(User)
+                            .values({
+                                username: req.body.username,
+                                password: hashPassword
+                            })
+                            .execute();
+                        res.status(201).json({ status: "success", msg: "User successfuly created" })
+                    }
+                } catch (e) {
+                    res.status(500).json({ status: "Db error", msg: "unable to find user in db" })
+                }
             })
             .catch(() => res.status(500).json({ status: "Hashing Failed", msg: "Password Hashing failed" }));
     
@@ -52,15 +58,16 @@ export default {
         })
     },
 
-    fetchUser: () => (req: Request, res: Response) => {
-        const user = req.session.passport?.user; 
-        if (user) {
-            db.query("SELECT * FROM bolt_user WHERE id = $1;", [user])
-                .then((result) => {
-                    if (result.rows.length == 0) res.status(404).json({ status: "not found", msg: "user not found" })
-                    else res.status(200).json({ status: "success", user: result.rows[0] })
-                })
-                .catch(err => res.status(500).json({ status: "Db error", msg: "unable to fetch user" }))
+    fetchUser: () => async (req: Request, res: Response) => {
+        const userId = req.session.passport?.user; 
+        if (userId) {
+            try {
+                const user = await UserRepository.findOne({ where: { id: userId } });
+                if (user) res.status(200).json({ status: "success", user: user });
+                else res.status(404).json({ status: "not found", msg: "user not found" })
+            } catch (e) {
+                res.status(500).json({ status: "Db error", msg: "unable to fetch user" })
+            }
         } else res.status(403).json({ status: "forbidden", msg: "You are not logged in" })
     }
 }
