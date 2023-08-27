829237c629e352f085e4cc162f9f398f1e0d0a32
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2023-08-27 22:31:25 +0800

    integrated typeORM

--- server/src/controllers/task.controller.ts
+++ server/src/controllers/task.controller.ts
@@ -1,7 +1,7 @@
-import db from "../model";
 import { Request, Response } from "express";
 import { z } from "zod";
 import { addTaskDb, deleteTaskDb, updateTaskDb } from "../model/task.db";
+import { PgTransaction, TaskRepository } from "../model/setup";
 
 const getAllTasks = (req: Request, res: Response) => {
     if (!req.isAuthenticated() || !req.session.passport?.user) 
@@ -9,12 +9,15 @@ const getAllTasks = (req: Request, res: Response) => {
             status: "forbidden", msg: "unable to access resource as an unatheticated user"
         }); 
     else {
-        db.query("SELECT * FROM task WHERE user_id = $1 ORDER BY duedate, task_order, completed_order;", [req.session.passport.user])
-            .then(result => res.status(200).json({
+        TaskRepository
+            .createQueryBuilder("task")
+            .where("task.user_id = :id", { id: req.session.passport.user })
+            .getMany()
+            .then(tasks => res.status(200).json({
                 status: "success",
                 msg: "tasks found",
-                results: result.rows.length,
-                tasks: result.rows
+                results: tasks.length,
+                tasks: tasks
             }))
             .catch(() => res.status(500).json({ status: "Db error", msg: "unable to fetch tasks" }))
     }
@@ -27,12 +30,16 @@ const getTask = (req: Request, res: Response) => {
             status: "forbidden", msg: "unable to access resource as an unatheticated user"
         });
     else {
-        db.query("SELECT * FROM task WHERE user_id = $1 AND id = $2;", [userId, req.params.id])
+        TaskRepository
+            .createQueryBuilder("task")
+            .where("task.id = :taskId", {taskId: req.params.id})
+            .where("task.user_id = :userId", {userId: userId})
+            .getOne()
             .then(result => res.status(200).json({
                 status: "success",
-                msg: "task found",
-                results: result.rows.length,
-                tasks: result.rows
+                msg: "tasks found",
+                results: result ? 1 : 0,
+                tasks: [result]
             }))
             .catch(() => res.status(500).json({ status: "Db error", msg: "unable to fetch tasks" }))
     }
@@ -49,17 +56,11 @@ const addTask = async (req: Request, res: Response) => {
             status: "forbidden", msg: "unable to access resource as an unatheticated user"
         });
     else {
-        const client = await db.getClient();
-
-        try {
-            await client.query("BEGIN");
+        PgTransaction(async (entityManager) => {
             const data = AddTaskConstraint.parse(req.body);
-
-            // set order string to task_order by default
-            const order_string = "task_order"
-
+            const order_string = "task_order";
             await addTaskDb(
-                client,
+                entityManager,
                 userId,
                 {
                     name: data.name,
@@ -69,46 +70,35 @@ const addTask = async (req: Request, res: Response) => {
                 order_string,
                 null
             )
-
-            await client.query("COMMIT")
             res.status(203).json({ msg: "task successfully added" })
-        } catch (e) {
-            await client.query("ROLLBACK");
+        })
+        .catch(e => {
             console.log("Insert task error")
             console.log(e)
             res.status(500).json({ msg: "db error" })
-        } finally {
-            client.release();
-        }
+        })
     }
 }
 
 const UpdateTaskConstraint = z.object({ name: z.string(), completed: z.boolean() })
 const updateTask = async (req: Request, res: Response) => {
     const userId = req.session.passport?.user;
-    const client = await db.getClient();
     const taskId = req.params.id;
     if (!req.isAuthenticated() || !userId) 
         res.status(403).json({
             status: "forbidden", msg: "unable to access resource as an unatheticated user"
         });
     else {
-        try {
-            await client.query("BEGIN");
+        PgTransaction(async (entityManager) => {
             const data = UpdateTaskConstraint.parse(req.body);
-
-            await updateTaskDb(client, userId, taskId, data);
-
-            await client.query("COMMIT");
-            res.status(200).json({ msg: "task updated" })
-            
-        } catch (e) {
-            client.query("ROLLBACK");
-            console.log(e);
+            await updateTaskDb(entityManager, userId, taskId, data);
+            res.status(203).json({ msg: "task successfully updated" })
+        })
+        .catch(e => {
+            console.log("update task error")
+            console.log(e)
             res.status(500).json({ msg: "db error" })
-        } finally {
-            client.release();
-        }
+        })
     }
 }
 
@@ -127,17 +117,21 @@ const fetchAllOverdueCategories = async (req: Request, res: Response) => {
         try {
             
             const data = FetchBodyConstraint.parse(req.body)
-            const duedates = await db.query(
-                "SELECT DISTINCT duedate FROM task WHERE user_id = $1 AND duedate < $2 AND completed = false ORDER BY duedate;", 
-                [userId, data.date]
-            ).then(res => res.rows.map(item => {
-                const date = new Date(item.duedate);
-                date.setUTCHours(0);
-                date.setUTCMinutes(0);
-                date.setUTCSeconds(0);
-                date.setUTCMilliseconds(0);
-                return { operator: "=", isCompleted: false, date: date.toJSON() }
-            }))
+            const duedates = await TaskRepository
+                .createQueryBuilder("task")
+                .where(
+                    "user_id = :userId AND duedate < :duedate AND completed = :completed",
+                    { userId, duedate: data.date, completed: false }
+                )
+                .getMany()
+                .then(res => res.map(item => {
+                    const date = new Date(item.duedate);
+                    date.setUTCHours(0);
+                    date.setUTCMinutes(0);
+                    date.setUTCSeconds(0);
+                    date.setUTCMilliseconds(0);
+                    return { operator: "=", isCompleted: false, date: date.toJSON() }
+                }))
             res.status(200).json({ count: duedates.length, category: duedates })
         } catch (e) {
             console.log(e)
@@ -155,19 +149,30 @@ const fetchAllCompletedCategories = async (req: Request, res: Response) => {
     
     else {
         try {
-            
             const data = FetchBodyConstraint.parse(req.body)
-            const duedates = await db.query(
-                "SELECT DISTINCT duedate FROM task WHERE user_id = $1 AND duedate < $2 AND completed = true ORDER BY duedate;", 
-                [userId, data.date]
-            ).then(res => res.rows.map(item => {
-                const date = new Date(item.duedate);
-                date.setUTCHours(0);
-                date.setUTCMinutes(0);
-                date.setUTCSeconds(0);
-                date.setUTCMilliseconds(0);
-                return { operator: "=", isCompleted: true, date: date.toJSON() }
-            }))
+            
+            const duedates = await TaskRepository
+                .createQueryBuilder("task")
+                .distinctOn(["task.duedate"])
+                .where(
+                    "user_id = :userId AND completed = :completed",
+                    {
+                        userId,
+                        duedate: data.date,
+                        completed: true
+                    }
+                )
+                .getMany()
+                .then(res => res.map(item => {
+                    console.log(item)
+                    const date = new Date(item.duedate);
+                    date.setUTCHours(0);
+                    date.setUTCMinutes(0);
+                    date.setUTCSeconds(0);
+                    date.setUTCMilliseconds(0);
+                    return { operator: "=", isCompleted: true, date: date.toJSON() }
+                }))
+            console.log(duedates)
             res.status(200).json({ count: duedates.length, category: duedates })
         } catch (e) {
             console.log(e)
@@ -183,12 +188,15 @@ const fetchTaskByDate = async (req: Request, res: Response) => {
             status: "forbidden", msg: "unable to access resource as an unatheticated user"
         });
     else {
-        db.query("SELECT id, name, duedate, completed FROM task WHERE user_id = $1 AND duedate = $2;", [userId, req.params.dateString])
+        TaskRepository
+            .createQueryBuilder("task")
+            .where("user_id = :userId AND duedate = :duedate", {userId, duedate: req.params.dateString})
+            .getMany()
             .then(result => res.status(200).json({
                 status: "success",
                 msg: "task found",
-                results: result.rows.length,
-                tasks: result.rows
+                results: result.length,
+                tasks: result
             }))
             .catch((err) => {
                 res.status(500).json({ status: "Db error", msg: "unable to fetch tasks" })
@@ -200,24 +208,20 @@ const fetchTaskByDate = async (req: Request, res: Response) => {
 const deleteTask = async (req: Request, res: Response) => {
     const userId = req.session.passport?.user;
     const taskId = req.params.id;
-    const client = await db.getClient();
     if (!req.isAuthenticated() || !userId) 
         res.status(403).json({
             status: "forbidden", msg: "unable to access resource as an unatheticated user"
         });
     else {
-        try {
-            await client.query("BEGIN");
-
-            const removedTask = await deleteTaskDb(client, userId, taskId);
-
-            await client.query("COMMIT");
+        PgTransaction(async (entityManager) => {
+            const removedTask = await deleteTaskDb(entityManager, userId, taskId);
             res.status(200).json({ msg: "task deleted", task: removedTask })
-        } catch (e) {
+        })
+        .catch(e => {
+            console.log("delete task error")
+            console.log(e)
             res.status(500).json({ msg: "db error" })
-        } finally {
-            client.release()
-        }
+        })
     }
 }
 
@@ -231,19 +235,22 @@ const fetchTaskByMonth = async (req: Request, res: Response) => {
     else {
         try {
             const [year, month] = YearMonthConstraint.parse(req.params.yearMonth.split("-"))
-            const result = await db.query(
-                `SELECT id, name, duedate, completed 
-                FROM task 
-                WHERE user_id = $1 AND EXTRACT(MONTH FROM duedate) = $2 AND EXTRACT(YEAR FROM duedate) = $3
-                ORDER BY completed, task_order, completed_order;`, 
-                [userId, month, year]
-            )
+            const result = await TaskRepository
+                .createQueryBuilder("task")
+                .where(
+                    "user_id = :userId AND EXTRACT(MONTH FROM duedate) = :month AND EXTRACT(YEAR FROM duedate) = :year", 
+                    {userId, month, year}
+                )
+                .orderBy("task.completed")
+                .addOrderBy("task.task_order")
+                .addOrderBy("task.completed_order")
+                .getMany()
 
             res.status(200).json({
                 status: "success",
                 msg: "task found",
-                results: result.rows.length,
-                tasks: result.rows
+                results: result.length,
+                tasks: result
             })
 
         } catch (e) {
